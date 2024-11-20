6f3e2c16a89206bab2e97c192cfcb7c72dde3135
Laria Leonardo Gabriel <90394909+pLaria@users.noreply.github.com>
2024-11-20 08:21:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,35 @@
+// Inicializar partículas
+particlesJS("particles-js", {
+    particles: {
+        number: { value: 100, density: { enable: true, value_area: 800 } },
+        color: { value: "#ffffff" },
+        shape: { type: "circle" },
+        opacity: { value: 0.5 },
+        size: { value: 3 },
+        line_linked: { enable: true, distance: 150, color: "#ffffff", opacity: 0.4, width: 1 },
+        move: { enable: true, speed: 2 }
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: { enable: true, mode: "repulse" },
+            onclick: { enable: true, mode: "push" }
+        },
+        modes: {
+            repulse: { distance: 100 },
+            push: { particles_nb: 4 }
+        }
+    },
+    retina_detect: true
+});
+
+// Menú hamburguesa
 const ham = document.querySelector('.ham');
 const enlaces = document.querySelector('.enlaces-menu');
 const barras = document.querySelectorAll('.ham span');
 
 ham.addEventListener('click', () => {
     enlaces.classList.toggle('activado');
-    barras.forEach(child => {child.classList.toggle('animado')});
+    barras.forEach(child => { child.classList.toggle('animado'); });
     ham.classList.toggle('girar');
 });
-
-window.addEventListener('load',() =>{
-    this.document.getElementById('loader').classList.toggle('loader2')
-})
